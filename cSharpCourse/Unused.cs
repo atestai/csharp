34f7a85126c3ee7c404611ded26f3c5278c14568
atestai <78636190+atestai@users.noreply.github.com>
2025-10-29 08:33:20 +0100

    Refactor: Simplify and enhance the TestClasses method in Unused class for better readability and functionality

--- cSharpCourse/Unused.cs
+++ cSharpCourse/Unused.cs
@@ -7,74 +7,73 @@ public static class Unused
     {
         Console.WriteLine("This is a test method in Unused class.");
 
+        Cat cat = new()
+        {
+            Name = "Whiskers",
+            Age = 3,
+            Species = "Felis catus"
+        };
 
-            Cat cat = new()
-            {
-                Name = "Whiskers",
-                Age = 3,
-                Species = "Felis catus"
-            };
+        cat.Sleep();
+        cat.AnimalSound();
+        cat.Eat();
 
-            cat.Sleep();
-            cat.AnimalSound();
-            cat.Eat();
+        Console.WriteLine($"Cat Name: {cat.Name}, Age: {cat.Age}, Species: {cat.Species}");
 
-            Console.WriteLine($"Cat Name: {cat.Name}, Age: {cat.Age}, Species: {cat.Species}");
+        cat.Log("Cat log entry");
+        cat.Log("Another cat log entry");
 
-            cat.Log("Cat log entry");
-            cat.Log("Another cat log entry");
-
-            foreach (var item in cat.ListLogs)
-            {
-                Console.WriteLine(item);
-            }
-
-
-            Animal dog = new Dog();  // Create a Dog object
-            dog.Sleep();
-            dog.Eat();
-            dog.Log("Dog log entry");
-
-            Farm myFarm = new()
-            {
-                Name = "Green Farm",
-                Location = "Texas",
-                Owner = new Person("Alice", "Johnson") { Id = 10, Age = 45 }
-            };
-            
-            myFarm.AddAnimal(cat);
-            myFarm.AddAnimal(dog);
-            myFarm.AddAnimal(new Pig());
-            myFarm.AddAnimal(new Lion());
-            myFarm.AddAnimal(new Lion());
+        foreach (var item in cat.ListLogs)
+        {
+            Console.WriteLine(item);
+        }
 
 
-            myFarm.MakeAllSounds();
+        Animal dog = new Dog();  // Create a Dog object
+        dog.Sleep();
+        dog.Eat();
+        dog.Log("Dog log entry");
 
-            return;
-            Person father = new("John", "Doe");
-            Person mother = new("Jane", "Smith");
-            Person kid1 = father with { FirstName = "Jimmy" };
-            Person kid2 = mother with { FirstName = "Jenny" };
+        Farm myFarm = new()
+        {
+            Name = "Green Farm",
+            Location = "Texas",
+            Owner = new Person("Alice", "Johnson") { Id = 10, Age = 45 }
+        };
 
-            Person grandpa = new("Jimmy", "Doe");
+        myFarm.AddAnimal(cat);
+        myFarm.AddAnimal(dog);
+        myFarm.AddAnimal(new Pig());
+        myFarm.AddAnimal(new Lion());
+        myFarm.AddAnimal(new Lion());
 
-            var familyMembers = new[]
-            {
+
+        myFarm.MakeAllSounds();
+
+        return;
+        Person father = new("John", "Doe");
+        Person mother = new("Jane", "Smith");
+        Person kid1 = father with { FirstName = "Jimmy" };
+        Person kid2 = mother with { FirstName = "Jenny" };
+
+        Person grandpa = new("Jimmy", "Doe");
+
+        var familyMembers = new[]
+        {
                 father,
                 mother,
                 kid1,
                 kid2
             };
 
-            foreach (var p in familyMembers)
-            {
-                Console.WriteLine($"Person: {p.FirstName} {p.LastName}");
-            }
+        foreach (var p in familyMembers)
+        {
+            Console.WriteLine($"Person: {p.FirstName} {p.LastName}");
+        }
 
-            Console.WriteLine($"Are the kids equal? {grandpa == kid1}");
-            Console.WriteLine($"Are the kids equal? {grandpa == kid2}");
-    }   
+        Console.WriteLine($"Are the kids equal? {grandpa == kid1}");
+        Console.WriteLine($"Are the kids equal? {grandpa == kid2}");
+    }
 
     static TimeSpan WhileDemo(long number, out TimeSpan duration)
     {
